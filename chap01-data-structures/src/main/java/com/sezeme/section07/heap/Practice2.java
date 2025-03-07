919251edf70760ae7572dd42afcf5c0fd6d838fd
sezeme <stellaa223@gmail.com>
2025-03-07 18:22:17 +0900

    data-structures

---
+++ chap01-data-structures/src/main/java/com/sezeme/section07/heap/Practice2.java
@@ -0,0 +1,9 @@
+package com.sezeme.section07.heap;
+
+public class Practice2 {
+
+    public long solution(String input) {
+        String[] parsedInput = input.split("\n");
+        return 0;
+    }
+}
