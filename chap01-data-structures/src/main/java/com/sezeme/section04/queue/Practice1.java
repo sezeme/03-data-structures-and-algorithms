3d02fd31789122813393153d9a5912fc3d6edeb9
sezeme <stellaa223@gmail.com>
2025-03-11 13:19:16 +0900

    commit

---
+++ chap01-data-structures/src/main/java/com/sezeme/section04/queue/Practice1.java
@@ -0,0 +1,11 @@
+package com.sezeme.section04.queue;
+
+import java.io.IOException;
+
+public class Practice1 {
+    public String solution(int n, int k) throws IOException {
+
+        return null;
+    }
+
+}
